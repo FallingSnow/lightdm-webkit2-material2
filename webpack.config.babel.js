534d624de5f21c7e7640b4e0ad1c3e2c0683f793
Ayrton Sparling <ayrton@sparling.us>
2017-01-01 23:00:48 -0800

    Upgrade to happypack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,13 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
+import os from 'os';
+import fs from 'fs';
+let HappyPack = require('happypack');
+let happyThreadPool = HappyPack.ThreadPool({
+    size: os.cpus().length + 1
+});
+const happyTempDir = path.resolve(os.tmpdir(), '.happypack');
 
 const ENV = process.env.NODE_ENV || 'development';
 
@@ -19,7 +26,8 @@ const config = {
         modulesDirectories: [
             path.resolve(__dirname, "node_modules"),
             'node_modules'
-        ]
+        ],
+        unsafeCache: true
     },
 
     module: {
@@ -30,38 +38,113 @@ const config = {
         }],
         loaders: [{
             test: /\.(xml|html|txt|md)$/,
-            loader: 'raw'
+            loader: 'happypack/loader?id=raw'
+        // }, {
+        //     test: /\.(jpe?g|png|gif|svg)$/i,
+        //     loader: 'happypack/loader?id=image'
+        }, {
+            test: /\.json$/,
+            loader: 'happypack/loader?id=json'
+        }, {
+            test: /\.css$/,
+            loader: "happypack/loader?id=css"
+        }, {
+            test: /\.less$/,
+            loader: "happypack/loader?id=less"
+        }, {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'happypack/loader?id=jsx'
+        // }, {
+        //     test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        //     loader: "happypack/loader?id=woff"
+        }, {
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "happypack/loader?id=file"
+        // }, {
+        //     test: /\.(xml|html|txt|md)$/,
+        //     loader: 'raw'
         }, {
             test: /\.(jpe?g|png|gif|svg)$/i,
             loaders: [
                 'file?hash=sha512&digest=hex&name=[hash].[ext]',
                 'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
             ]
-        }, {
-            test: /\.json$/,
-            loaders: ["json-loader"]
-        }, {
-            test: /\.css$/,
-            loader: "style-loader!css-loader"
-        }, {
-            test: /\.less$/,
-            loader: "style-loader!css-loader!less-loader"
-        }, {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel'
+        // }, {
+        //     test: /\.json$/,
+        //     loaders: ["json-loader"]
+        // }, {
+        //     test: /\.css$/,
+        //     loader: "style-loader!css-loader"
+        // }, {
+        //     test: /\.less$/,
+        //     loader: "style-loader!css-loader!less-loader"
+        // }, {
+        //     test: /\.jsx?$/,
+        //     exclude: /node_modules/,
+        //     loader: 'babel'
         }, {
             test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
             loader: "url-loader?limit=10000&minetype=application/font-woff"
-        }, {
-            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: "file-loader"
+        // }, {
+        //     test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        //     loader: "file-loader"
         }]
     },
 
     plugins: ([
+            new HappyPack({
+                id: 'raw',
+                threadPool: happyThreadPool,
+                loaders: ['raw'],
+                tempDir: happyTempDir
+            }),
+            new HappyPack({
+                id: 'jsx',
+                threadPool: happyThreadPool,
+                loaders: ['babel'],
+                tempDir: happyTempDir
+            }),
+            new HappyPack({
+                id: 'json',
+                threadPool: happyThreadPool,
+                loaders: ['json-loader'],
+                tempDir: happyTempDir
+            }),
+            new HappyPack({
+                id: 'file',
+                threadPool: happyThreadPool,
+                loaders: ['file-loader'],
+                tempDir: happyTempDir
+            }),
+            new HappyPack({
+                id: 'less',
+                threadPool: happyThreadPool,
+                loaders: ['style-loader!css-loader!less-loader'],
+                tempDir: happyTempDir
+            }),
+            new HappyPack({
+                id: 'css',
+                threadPool: happyThreadPool,
+                loaders: ['style-loader!css-loader'],
+                tempDir: happyTempDir
+            }),
+            new HappyPack({
+                id: 'image',
+                threadPool: happyThreadPool,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ],
+                tempDir: happyTempDir
+            }),
+            new HappyPack({
+                id: 'woff',
+                threadPool: happyThreadPool,
+                loaders: ['url-loader?limit=10000&minetype=application/font-woff'],
+                tempDir: happyTempDir
+            }),
             new webpack.NoErrorsPlugin(),
-            new webpack.optimize.DedupePlugin(),
             new webpack.DefinePlugin({
                 'process.env': JSON.stringify({
                     NODE_ENV: ENV
@@ -88,7 +171,7 @@ const config = {
     devtool: '#inline-source-map',
 
     devServer: {
-        port: process.env.PORT || 8080,
+        port: process.env.PORT || 8084,
         host: '0.0.0.0',
         colors: true,
         publicPath: '/',
