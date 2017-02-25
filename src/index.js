e906b512aa404403e86d0703d30a4b21d94352b5
Ayrton Sparling <ayrton@sparling.us>
2017-02-25 12:30:51 -0800

    Added potential fix for lightdm not loading in time Also included error message

--- src/index.js
+++ src/index.js
@@ -7,50 +7,61 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-LightDMMock = require("../LightDMMock/mock/LightDMMock.js")
-if (!("lightdm" in window)) {
-    var LightDMMock = LightDMMock || {};
-    window.lightdm = new LightDMMock(true, 0, false);
-    window.lightdm.languages.push({code: 'es-ES', name: 'Spanish', territory: null});
-    window.lightdm.languages.push({code: 'af-AF', name: 'Africanese', territory: null});
-    window.lightdm.languages.push({code: 'ar-SR', name: 'Arabic', territory: null});
-    window.lightdm.languages.push({code: 'ja-JP', name: 'Japanese', territory: null});
-    window.lightdm.languages.push({code: 'zh-HK', name: 'Chinese', territory: null});
-    window.lightdm.languages.push({code: 'he-IS', name: 'Hebrew', territory: null});
-    for (let user of lightdm.users) {
-        user.username = user.name;
-    }
-    console.info('Mock users loaded:',lightdm.users)
-    lightdm.can_shutdown = lightdm.can_restart = lightdm.can_suspend = lightdm.can_hibernate = true;
+if (typeof lightdm !== 'undefined')
+    lightdm = undefined;
+setTimeout(() => {
 
-    if (!("config" in window)) {
-        window.config = {
-            get_bool: function(key) {
-                return true;
-            },
-            get_str: function(key) {
-                return "";
-            },
-            get_num: function(key) {
-                return 0;
-            }
-        };
-    }
-    if (!("greeterutil" in window)) {
-        window.greeterutil = {
-            dirlist: function(dir) {
-                return undefined;
+    if (!("lightdm" in window)) {
+        if (process.env.NODE_ENV !== 'development') {
+            document.body.innerHTML += '<h1 style="background-color: white; color: red;">Lightdm did not load</h1>';
+            throw new Error('lightdm did not load in time');
+        } else {
+            const LightDMMock = require("../LightDMMock/mock/LightDMMock.js");
+            // const LightDMMock = require("../LightDMMock/src/LightDMMock.js");
+            if (!("lightdm" in window)) {
+                window.lightdm = new LightDMMock(true, 0, false);
+                window.lightdm.languages.push({code: 'es-ES', name: 'Spanish', territory: null});
+                window.lightdm.languages.push({code: 'af-AF', name: 'Africanese', territory: null});
+                window.lightdm.languages.push({code: 'ar-SR', name: 'Arabic', territory: null});
+                window.lightdm.languages.push({code: 'ja-JP', name: 'Japanese', territory: null});
+                window.lightdm.languages.push({code: 'zh-HK', name: 'Chinese', territory: null});
+                window.lightdm.languages.push({code: 'he-IS', name: 'Hebrew', territory: null});
+                for (let user of lightdm.users) {
+                    user.username = user.name;
+                }
+                console.info('Mock users loaded:', lightdm.users)
+                lightdm.can_shutdown = lightdm.can_restart = lightdm.can_suspend = lightdm.can_hibernate = true;
+
+                if (!("config" in window)) {
+                    window.config = {
+                        get_bool: function(key) {
+                            return true;
+                        },
+                        get_str: function(key) {
+                            return "";
+                        },
+                        get_num: function(key) {
+                            return 0;
+                        }
+                    };
+                }
+                if (!("greeterutil" in window)) {
+                    window.greeterutil = {
+                        dirlist: function(dir) {
+                            return undefined;
+                        }
+                    }
+                }
             }
         }
     }
-}
+    init();
+}, 10);
 
 function init() {
     let Main = require('./components/Main.jsx').default;
     render(< Main / >, document.getElementById('wrapper'));
 }
-
-init();
 
 if (module.hot) {
     module.hot.accept('./components/Main.jsx', () => requestAnimationFrame(() => {
